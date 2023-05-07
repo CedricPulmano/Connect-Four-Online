13df814fbda64b929b4e337c9c18361a0bfa9967
SeanN <seannoh1654@yahoo.ca>
2023-05-06 21:12:02 -0700

    Removed Scroll Wheel on Game Page

--- client/src/pages/boardRoom/BoardRoom.js
+++ client/src/pages/boardRoom/BoardRoom.js
@@ -3,15 +3,13 @@ import "./BoardRoom.css";
 import Board from "../../components/Board";
 
 const BoardRoom = () => {
-    return (
-        <div className="game-page">
-            <header className="game-header"></header>
-            <main className="board-container">
-                <Board className="board"></Board>
-            </main>
-            <footer className="game-footer"></footer>
-        </div>
-    );
+  return (
+    <div className="game-page">
+      <main className="board-container">
+        <Board className="board"></Board>
+      </main>
+    </div>
+  );
 };
 
 export default BoardRoom;
