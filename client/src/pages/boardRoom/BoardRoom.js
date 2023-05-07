1800f7b442082092ea416e69a1feea073e1f0f6f
SeanN <seannoh1654@yahoo.ca>
2023-05-06 20:42:21 -0700

    Temp 2

--- client/src/pages/boardRoom/BoardRoom.js
+++ client/src/pages/boardRoom/BoardRoom.js
@@ -2,15 +2,15 @@ import React from "react";
 import Board from "../../components/Board";
 
 const BoardRoom = () => {
-    return (
-        <div className="game-page">
-            <header className="game-header"></header>
-            <main className="board-container">
-                <Board className="board"></Board>
-            </main>
-            <footer className="game-footer"></footer>
-        </div>
-    );
+  return (
+    <div className="game-page">
+      {/* <header className="game-header"></header> */}
+      <main className="board-container">
+        <Board className="board"></Board>
+      </main>
+      <section className="game-status"></section>
+    </div>
+  );
 };
 
 export default BoardRoom;
