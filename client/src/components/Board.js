a934708f2c248996f8506b5fdb8674d536419ad0
SeanN <seannoh1654@yahoo.ca>
2023-05-08 16:41:43 -0700

    Made Position Stateless and Board have State

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,4 +1,5 @@
 import Position from "./Position";
+import { useState } from "react";
 import "./Board.css";
 
 // 2d array 6x7 matrix
@@ -8,30 +9,40 @@ import "./Board.css";
 // create 6 arrays, each array being a column
 
 const Board = () => {
-    function createRow() {
-        return (
-            <>
-                <Position />
-                <Position />
-                <Position />
-                <Position />
-                <Position />
-                <Position />
-                <Position />
-            </>
-        );
-    }
+  const [board, setBoard] = useState([
+    ["white", "white", "white", "white", "white", "white", "white"],
+    ["white", "white", "white", "white", "white", "white", "white"],
+    ["white", "white", "white", "white", "white", "white", "white"],
+    ["white", "white", "white", "white", "white", "white", "white"],
+    ["white", "white", "white", "white", "white", "white", "white"],
+    ["white", "white", "white", "white", "white", "white", "white"],
+  ]);
 
+  function createRow(columnArray) {
     return (
-        <div className="board">
-            <div className="column-one column">{createRow()}</div>
-            <div className="column-two column">{createRow()}</div>
-            <div className="column-three column">{createRow()}</div>
-            <div className="column-four column">{createRow()}</div>
-            <div className="column-five column">{createRow()}</div>
-            <div className="column-six column">{createRow()}</div>
-        </div>
+      <>
+        <Position cursorType="pointer" pieceColor={columnArray[0]} />
+        <Position pieceColor={columnArray[1]} />
+        <Position pieceColor={columnArray[2]} />
+        <Position pieceColor={columnArray[3]} />
+        <Position pieceColor={columnArray[4]} />
+        <Position pieceColor={columnArray[5]} />
+        <Position pieceColor={columnArray[6]} />
+      </>
     );
+  }
+
+  // include event handlers in the board columns
+  return (
+    <div className="board">
+      <div className="column-one column">{createRow(board[0])}</div>
+      <div className="column-two column">{createRow(board[1])}</div>
+      <div className="column-three column">{createRow(board[2])}</div>
+      <div className="column-four column">{createRow(board[3])}</div>
+      <div className="column-five column">{createRow(board[4])}</div>
+      <div className="column-six column">{createRow(board[5])}</div>
+    </div>
+  );
 };
 
 export default Board;
