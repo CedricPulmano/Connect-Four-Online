b3963fcdf44c92c277cbbd45b23fa699a6cd2eea
SeanN <seannoh1654@yahoo.ca>
2023-05-08 22:01:49 -0700

    Removed outdated code in Position.js

--- client/src/components/Position.js
+++ client/src/components/Position.js
@@ -6,33 +6,11 @@ const Position = ({ pieceColor, cursorType }) => {
     cursorType = "default";
   }
 
-  // const [content, setContent] = useState("white");
-
-  // function playerSelect1() {
-  //   setContent("yellow");
-  // }
-
-  // function playerSelect2() {
-  //   setContent("red");
-  // }
-
-  // function randomizePlayer() {
-  //   if (content !== "white") {
-  //     return;
-  //   }
-  //   if (Math.random() > 0.5) {
-  //     playerSelect1();
-  //   } else {
-  //     playerSelect2();
-  //   }
-  // }
-
   return (
     <div className="position-space">
       <div
         className="piece"
         style={{ backgroundColor: pieceColor, cursor: cursorType }}
-        // onClick={randomizePlayer}
       ></div>
     </div>
   );
